5a42a695a1bd83d09c56b4104ebb856dff84cc38
BimboBabatola <bimbo@woodcoreapp.com>
2023-05-06 03:16:49 +0100

    update

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-import Flask
+from flask import Flask, jsonify, request
+from util.writer import SalesReport
+
+app = Flask(__name__)
+
+
+@app.post("/generate_report")
+def entryPoint():
+    SalesReport().generate()
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+# python3 -m flask run
+# FLASK_APP=app.py FLASK_ENV=development python3 -m flask run
+    
--- util/writer.py
+++ util/writer.py
@@ -1,4 +1,4 @@
-from util import data
+import data
 import pandas as pd
 
 class SalesReport:
@@ -13,7 +13,7 @@ class SalesReport:
 
     def generate(self):
         
-        writer = pd.ExcelWriter('report.xlxs', engine = "xlsxwriter", 
+        writer = pd.ExcelWriter('report.xlsx', engine = "xlsxwriter", 
                                                 engine_kwargs={'options': {'strings_to_numbers': False}})
         workbook  = writer.book
 
@@ -25,19 +25,25 @@ class SalesReport:
         #text formats
         title_text = workbook.add_format({'bold': True, 'font_size': 20, 'align':'center'})
         subtitle_text = workbook.add_format({'bold': True, 'font_size': 16, 'align':'center'})
-        column_header_text = workbook.add_format({'bold': True, 'font_size': 11, 'align':'center'})
+        column_header_text = workbook.add_format({'bold': True, 'font_size': 14, 'align':'center'})
+        bold_text = workbook.add_format({'bold': True, 'font_size': 14,})
+        regular_text= workbook.add_format({'font_size': 14,})
         # date format
-        date_format = workbook.add_format({'num_format': 'dd-mmm-yyyy', 'font_size': 11, 'align':'right'})
+        date_format = workbook.add_format({'num_format': 'dd-mmm-yyyy', 'font_size': 14, 'align':'right'})
         #number format
-        number = workbook.add_format({'num_format': '', 'font_size': 11, 'align':'right'})
-        bold_number = workbook.add_format({'bold': True, 'num_format': '', 'font_size': 11, 'align':'right'})
+        number = workbook.add_format({'num_format': '#,##0.00_);(#,##0.00)', 'font_size': 11})
+        bold_number = workbook.add_format({'bold': True, 'num_format': '#,##0.00_);(#,##0.00)', 'font_size': 14, 'align':'right'})
 
         #report body
         report_data = data.sales_data()
+
+        #set report start row and column
+        start_row = 3
+        start_col = 0
         #change index to start from 1
         report_data.index = [i for i in range(1, len(report_data) + 1)]
         report_data.to_excel(writer, sheet_name='Sales', index=True, 
-                                    startrow=4, startcol=0, header=True)
+                                    startrow=start_row, startcol=start_col, header=True)
         
         #hide gridlines
         writer.sheets['Sales'].hide_gridlines(2)
@@ -47,10 +53,28 @@ class SalesReport:
         writer.sheets['Sales'].merge_range("A2:F2", "1 JAN 2023 - 3 JAN 2023", subtitle_text)
         
         #Profit column
-        writer.sheets['Sales'].write('F5', 'Profit', column_header_text)
+        writer.sheets['Sales'].write(f'F{start_row+1}', 'Profit', column_header_text)
         #calculate profit
-        for i in range(6, 6 + len(report_data) + 1):
+        for i in range(start_row+2, start_row+2 + len(report_data)):
             writer.sheets['Sales'].write(f'F{i}', f'=E{i}-D{i}', number)
 
+        #TOTAL
+        last_row = len(report_data) + start_row + 2
+        writer.sheets['Sales'].write(f'C{last_row}', 'Total', bold_text)
+        writer.sheets['Sales'].write_formula(f'E{last_row}', f'=SUM(E{start_row+2}:E{last_row-1})', bold_number)
+        writer.sheets['Sales'].write_formula(f'F{last_row}', f'=SUM(F{start_row+2}:F{last_row-1})', bold_number)
+
+        #ADD BORDERS
+        writer.sheets['Sales'].conditional_format(start_row, 0, start_row, 5, {'type': 'no_blanks', 'format': thick_borders})
+        writer.sheets['Sales'].conditional_format(start_row, 0, start_row, 5, {'type': 'blanks', 'format': thick_borders})
+        writer.sheets['Sales'].conditional_format(start_row+1, 0, last_row-1, 5, {'type': 'no_blanks', 'format': thin_borders})
+        writer.sheets['Sales'].conditional_format(start_row+1, 0, last_row-1, 5, {'type': 'blanks', 'format': thin_borders})
+
+        #increase table font size
+        writer.sheets['Sales'].conditional_format(f'D{start_row+2}:E{last_row}', {'type': 'no_blanks', 'format': number})
 
 
+        workbook.close()
+
+
+SalesReport().generate()
